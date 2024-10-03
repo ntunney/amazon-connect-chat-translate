d97de4cb3b61e580ff8b44718bf85353e5ce79c5
ntunney <ntunney@users.noreply.github.com>
2024-10-03 10:53:36 -0400

    moving around the incoming call buttons

--- src/components/ccp.js
+++ src/components/ccp.js
@@ -216,7 +216,7 @@ const Ccp = () => {
                     {/* CCP window will load here */}
                     <div id="ccp-container" className="ccp-hidden"></div>
                     {/* Translate window will laod here. We pass the agent state to be able to use this to push messages to CCP */}
-                    <div id="chatroom" ><Chatroom session={agentChatSessionState}/> </div>
+                    <div id="chatroom" className="translate-container"><Chatroom session={agentChatSessionState}/> </div>
                 </Grid.Row>
             </Grid>
         </main>
