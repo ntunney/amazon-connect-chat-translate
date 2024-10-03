8a8848dd1a6d5380495f7fb65775c8c0a5ee8906
ntunney <ntunney@users.noreply.github.com>
2024-10-02 20:01:37 -0400

    readd cognito for now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Amplify }  from '@aws-amplify/core';
-// import { withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsconfig from './aws-exports';
 import React, { useState, useEffect } from 'react';
@@ -32,4 +32,4 @@ function App({ signOut, user }) {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
